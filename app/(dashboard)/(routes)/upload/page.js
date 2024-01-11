dfa84226cb20a9db1467f1209d4256e6cd967bd7
AdewoleCode <ademolaadewoleo1@gmail.com>
2024-01-11 11:02:19 +0100

    commit

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -66,7 +66,7 @@ const Upload = () => {
             userName: user?.fullName,
             password: "",
             id: docId,
-            shortURL: process.env.NEXT_PUBLIC_BASE_URL + 'viewFile/' + docId
+            shortURL: NEXT_PUBLIC_PRODUCTION_URL + 'viewFile/' + docId
         })
     }
 
